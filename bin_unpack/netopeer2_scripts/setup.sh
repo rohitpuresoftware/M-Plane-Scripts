2fa8c5108dee7b09ae12c6025b18acce0884e0a7
chendrashekarp <pabbaraju.chendrashekar@puresoftware.com>
2021-08-03 22:00:52 +0530

    yang-models-installation

--- bin_unpack/netopeer2_scripts/setup.sh
+++ bin_unpack/netopeer2_scripts/setup.sh
@@ -43,6 +43,12 @@ MODULES=(
 "ietf-ssh-server@2019-07-02.yang -e local-client-auth-supported"
 "ietf-tls-server@2019-07-02.yang -e local-client-auth-supported"
 "ietf-netconf-server@2019-07-02.yang -e ssh-listen -e tls-listen -e ssh-call-home -e tls-call-home"
+"ietf-interfaces@2018-02-20.yang"
+"ietf-ip@2018-02-22.yang"
+"ietf-yang-schema-mount@2019-01-14.yang"
+"ietf-network-instance@2019-01-21.yang"
+"ietf-restconf@2017-01-26.yang"
+"ietf-subscribed-notifications@2019-09-09.yang"
 )
 
 # functions
